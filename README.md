5d20e77142a6b623b28dd9c07612bc53e85d24e0
Melvin Courant <melvin.courant1@gmail.com>
2022-11-06 15:02:09 +0100

    Finished on desktop

---
+++ README.md
@@ -0,0 +1,8 @@
+Star Wars
+
+From Benjamin Code's challenge <br>
+
+Video of the challenge: <br>
+https://youtu.be/Ld97MuYMaQQ <br>
+Figma model: <br>
+https://www.figma.com/proto/xaisLkNZJvZXq6Ca8qO6El/UI_StarWars?node-id=172%3A243&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=172%3A243
--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ window.addEventListener("load", function () {
     const previousButton = document.querySelector("#previous");
     const nextButton = document.querySelector("#next");
     const slides = document.querySelectorAll("[data-slide]");
-    let currentSlide = 1;
+    const eight = document.querySelector(".first-number[data-slide = '1']");
+    let currentSlide = 0;
 
     previousButton.addEventListener("click", function () {
-        if (currentSlide !== 1) {
+        if (currentSlide !== 0) {
             currentSlide--;
 
             if (nextButton.className === "max") {
@@ -15,22 +16,32 @@ window.addEventListener("load", function () {
             const slidesActived = document.querySelectorAll(".active");
 
             slidesActived.forEach(function (slideActived) {
-                slideActived.className = slideActived.className.replace("active", "");
+                slideActived.className = slideActived.className.replace(" active", "");
+                slideActived.ariaHidden = "true";
             })
+
             slides.forEach(function (slide) {
                 if (slide.getAttribute("data-slide") === currentSlide.toString()) {
-                    slide.className = "active";
+                    slide.className = slide.className.replace("hide", "active");
+                    slide.ariaHidden = "false";
                 }
             });
 
-            if (currentSlide === 1) {
+            if (currentSlide === 0) {
                 previousButton.className = "max";
+                eight.className = eight.className.replace(" active", "");
+                eight.ariaHidden = "true";
+            }
+
+            if (currentSlide === 1) {
+                eight.className += " active";
+                eight.ariaHidden = "false";
             }
         }
     });
 
     nextButton.addEventListener("click", function () {
-        if (currentSlide !== 3) {
+        if (currentSlide !== 2) {
             currentSlide++;
 
             if (previousButton.className === "max") {
@@ -41,15 +52,20 @@ window.addEventListener("load", function () {
 
             slidesActived.forEach(function (slideActived) {
                 slideActived.className = slideActived.className.replace("active", "hide");
+                slideActived.ariaHidden = "true";
             })
+
             slides.forEach(function (slide) {
                 if (slide.getAttribute("data-slide") === currentSlide.toString()) {
-                    slide.className = "active";
+                    slide.className += " active";
+                    slide.ariaHidden = "false";
                 }
             });
 
-            if (currentSlide === 3) {
+            if (currentSlide === 2) {
                 nextButton.className = "max";
+                eight.className = eight.className.replace("hide", "active");
+                eight.ariaHidden = "false";
             }
         }
     });
